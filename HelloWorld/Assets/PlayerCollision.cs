ddffa5c6b27b0e6dfc79a918f1bb5559da7fe28c
ottomattas <otto.mattas@gmail.com>
2021-02-06 10:46:05 +0100

    Only print on Obstacle collision

--- HelloWorld/Assets/PlayerCollision.cs
+++ HelloWorld/Assets/PlayerCollision.cs
@@ -7,7 +7,11 @@ public class PlayerCollision : MonoBehaviour
     // Specify method for when object collides with something
     void OnCollisionEnter (Collision collisionInfo)
     {
+        if (collisionInfo.collider.name == "Obstacle")
+        {
+            Debug.Log("We hit an obstacle!");
+        }
         // Print message to console
-        Debug.Log(collisionInfo.collider.name);
+        //Debug.Log(collisionInfo.collider.name);
     }
 }
