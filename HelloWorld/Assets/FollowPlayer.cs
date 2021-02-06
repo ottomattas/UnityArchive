95dc4047973c83d0b4aa019e998930f2d0d3d2eb
ottomattas <otto.mattas@gmail.com>
2021-02-06 10:21:18 +0100

    Add camera offset to camera position transformation

--- HelloWorld/Assets/FollowPlayer.cs
+++ HelloWorld/Assets/FollowPlayer.cs
@@ -7,14 +7,15 @@ public class FollowPlayer : MonoBehaviour
 
     // Store the reference for the player
     public Transform player;
-    // Va
+    // Create a Vector3 for camera offset
     public Vector3 offset;
 
     // Update is called once per frame
     void Update()
     {
         // Transform the camera position to the position of our player
-        transform.position = player.position;
+        // and add the specified offset
+        transform.position = player.position + offset;
         // Display the position of our player
         //Debug.Log(player.position);
     }
