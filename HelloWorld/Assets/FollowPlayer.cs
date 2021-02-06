01619d02078f467ec4d44e5a3aa11ecaeaa64d3f
ottomattas <otto.mattas@gmail.com>
2021-02-06 10:12:28 +0100

    Display position of our player in console

---
+++ HelloWorld/Assets/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    // Store the reference for the player
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Display the position of our player
+        Debug.Log(player.position);
+    }
+}
