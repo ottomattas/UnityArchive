e4a40935a9a4179632c4b7cb3177b9ac1cde37d0
ottomattas <otto.mattas@gmail.com>
2021-02-09 18:32:11 +0100

    Write custom EndGame function

--- HelloWorld/Assets/Scripts/GameManager.cs
+++ HelloWorld/Assets/Scripts/GameManager.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Write custom function
+    public void EndGame ()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("GAME OVER!");
     }
 }
