677e1ba541551026a063dd2e682c50f859482fc6
ottomattas <otto.mattas@gmail.com>
2021-02-09 20:24:32 +0100

    Use SceneManager and build index to load next scene

--- HelloWorld/Assets/Scripts/LevelComplete.cs
+++ HelloWorld/Assets/Scripts/LevelComplete.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour
 {
     // Create a public method to load next level
     public void LoadNextLevel ()
     {
-        
+        // Use SceneManager to load the next scene
+        // based on currently active scene and the build index
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
