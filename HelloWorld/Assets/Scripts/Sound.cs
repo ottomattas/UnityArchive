ac0759ebbbaca2bb3117455c722050a929384d71
ottomattas <otto.mattas@gmail.com>
2021-02-12 18:55:04 +0100

    Comment on creating the sound class

--- HelloWorld/Assets/Scripts/Sound.cs
+++ HelloWorld/Assets/Scripts/Sound.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+// Create a sound class
 public class Sound
 {
     
